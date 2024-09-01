f50ae2627018586ae1df33a898413a36b2681452
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-09-01 12:03:00 +0530

    Today I coded a lot completed tcp

--- tcp/server.cpp
+++ tcp/server.cpp
@@ -14,5 +14,29 @@ int main() {
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    
+    address.sin_port = htons(8080);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0){
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(server_fd, 3) < 0){
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&address)) < 0){
+        perror("accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[1024] = {0};
+    read(new_socket, buffer, 1024); 
+    std::cout << "Received Messages: " << buffer << std::endl;
+
+    const char* message = "Hello from server!";
+    send(new_socket, message, strlen(message), 0);
+
+    return 0;
 }
